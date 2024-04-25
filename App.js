b032f72276affcf316b2e1eed1ebdb9912218d34
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-04-25 12:40:44 +0500

    set up project and create tab bar

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import SearchScreen from "./src/screens/SearchScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import HistoryScreen from "./src/screens/HistoryScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === "Искать") {
+                iconName = focused ? "search" : "search";
+              } else if (route.name === "Поездки") {
+                iconName = focused ? "history" : "history";
+              } else {
+                iconName = focused ? "user-circle-o" : "user-circle-o";
+              }
+
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Искать" component={SearchScreen} />
+          <Tab.Screen name="Поездки" component={HistoryScreen} />
+          <Tab.Screen name="Профиль" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
