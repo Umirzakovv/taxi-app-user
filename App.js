da0c9e304577e63f14996a69007c923c75912ed6
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-04-30 14:03:55 +0500

    create chatScreen

--- App.js
+++ App.js
@@ -4,38 +4,42 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import SearchScreen from "./src/screens/SearchScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
 import HistoryScreen from "./src/screens/HistoryScreen";
+import ChatScreen from "./src/screens/Chat";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === "Искать") {
-                iconName = focused ? "search" : "search";
-              } else if (route.name === "Поездки") {
-                iconName = focused ? "history" : "history";
-              } else {
-                iconName = focused ? "user-circle-o" : "user-circle-o";
-              }
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Искать") {
+              iconName = focused ? "search" : "search";
+            } else if (route.name === "Поездки") {
+              iconName = focused ? "history" : "history";
+            } else if (route.name === "Входящие") {
+              iconName = focused ? "wechat" : "wechat";
+            } else {
+              iconName = focused ? "user-circle-o" : "user-circle-o";
+            }
 
-              return <Icon name={iconName} size={size} color={color} />;
-            },
-            tabBarStyle: {
-              backgroundColor: "#131514",
-              paddingBottom: 5,
-            },
-            tabBarActiveTintColor: "#0f95ca",
-            tabBarInactiveTintColor: "gray",
-          })}
-        >
-          <Tab.Screen name="Искать" component={SearchScreen} />
-          <Tab.Screen name="Поездки" component={HistoryScreen} />
-          <Tab.Screen name="Профиль" component={ProfileScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarStyle: {
+            backgroundColor: "#131514",
+            paddingBottom: 5,
+          },
+          tabBarActiveTintColor: "#0f95ca",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Искать" component={SearchScreen} />
+        <Tab.Screen name="Поездки" component={HistoryScreen} />
+        <Tab.Screen name="Входящие" component={ChatScreen} />
+        <Tab.Screen name="Профиль" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/screens/Chat/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Dimensions, Image, Text, View } from "react-native";
+const ChatScreen = () => {
+  const screenHeight = Dimensions.get("window").height;
+  return (
+    <View style={[{ height: screenHeight, backgroundColor: "#131514" }]}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={{ color: "#fff", fontSize: 16 }}>В процессе разработки</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ChatScreen;
